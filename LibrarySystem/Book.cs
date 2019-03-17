6557da6c377f6aba7909e2bdf3cd77c1ac934dd9
lijinmei <lijinmei@live.com>
2019-03-16 18:34:34 -0700

    initial sync

---
+++ LibrarySystem/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrarySystem
+{
+    class Book
+    {
+        public int BookID { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+        public string BookName { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Introduction { get; set; }
+    }
+}
+    
---
+++ LibrarySystem/Reader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrarySystem
+{
+    class Reader
+    {
+        #region properties
+
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public string IDType { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedDate{ get; set; }
+        public int TotalOfBorrowed { get; set; }
+        public decimal Fine { get; set; }
+        public string Notes { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public int BorrowBook (int BookID)
+        {
+            TotalOfBorrowed += 1;
+        }
+
+        public int ReturnBook (int BookID)
+        {
+            TotalOfBorrowed -= 1;
+        }
+
+        #endregion
+    }
+}
